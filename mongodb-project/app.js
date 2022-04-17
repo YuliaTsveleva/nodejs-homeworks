6b86ca54250f73ba5a60edaafb2100d3ba23719c
YuliaTsveleva <tsveleva.yulia@gmail.com>
2022-04-18 00:09:15 +0300

    add database connection

--- mongodb-project/app.js
+++
@@ -1,2 +0,0 @@
-/* 89DpPJvhtPdjOvlh password
-mongodb+srv://alaina:89DpPJvhtPdjOvlh@cluster0.g5rwq.mongodb.net/test */
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const app = require("./app");
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
+
+const DB_HOST =
+  "mongodb+srv://alaina:89DpPJvhtPdjOvlh@cluster0.g5rwq.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connected"))
+  .catch((error) => console.log(error.message));
