c2d9dce140db7f46f95141ba74093f7f6169ceec
YuliaTsveleva <tsveleva.yulia@gmail.com>
2022-04-19 00:51:04 +0300

    add .env

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const app = require("./app");
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
-
-const DB_HOST =
-  "mongodb+srv://alaina:89DpPJvhtPdjOvlh@cluster0.g5rwq.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
 
 mongoose
   .connect(DB_HOST)
-  .then(() => console.log("Database connected"))
-  .catch((error) => console.log(error.message));
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
