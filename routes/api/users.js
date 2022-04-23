d5dbf8c95c3a1bb5aaad2c623fa26810ba4a9dca
YuliaTsveleva <tsveleva.yulia@gmail.com>
2022-04-23 15:14:23 +0300

    add logout

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,4 +12,6 @@ router.post("/login", validation(joiLoginSchema), ctrlWrapper(ctrl.login));
 
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 
+router.get("/logout", auth, ctrlWrapper(ctrl.logout));
+
 module.exports = router;
