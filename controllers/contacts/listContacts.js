28464f9eadb89ac964995f704f6a4804c2d54c7e
YuliaTsveleva <tsveleva.yulia@gmail.com>
2022-04-23 14:33:58 +0300

    add pagination

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,10 +2,13 @@ const { Contact } = require("../../models");
 
 const listContacts = async (req, res) => {
   const { _id } = req.user;
-  const result = await Contact.find({ owner: _id }).populate(
-    "owner",
-    "_id email subscription"
-  );
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await Contact.find({ owner: _id }, "", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "_id email subscription");
 
   res.json({
     status: "success",
